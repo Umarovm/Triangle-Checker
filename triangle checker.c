fb07ccea36d79bfc43672a7fe6f9318548f95b58
Umarovm <giyasumarov@icloud.com>
2021-01-06 14:59:46 -0500

    Add files via upload

---
+++ triangle checker.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	long double a, b, c;
+
+	printf("Enter a's value: ");
+	scanf("%Lf", &a);
+	printf("Enter b's value: ");
+	scanf("%Lf", &b);
+	printf("Enter c's value: ");
+	scanf("%Lf", &c);
+	long double x = powl(a, 2) + powl(b, 2), y = powl(c, 2);
+	if (a + b > c) {
+		if (x == y)
+			printf("This is a right triangle.\n%Lf² + %Lf² = %Lf²\n", a, b, c);
+		else if (x > y)
+			printf("This is an acute triangle.\n%Lf² + %Lf² > %Lf²\n", a, b, c);
+		else
+			printf("This is an obtuse triangle.\n%Lf² + %Lf² < %Lf²\n", a, b, c);
+	}
+	else if (a + b < c)
+		printf("This is not a triangle.\n%Lf + %Lf < %Lf\n", a, b, c);
+	else
+		printf("This is not a triangle.\n%Lf + %Lf = %Lf\n", a, b, c);
+	return 0;
+}
